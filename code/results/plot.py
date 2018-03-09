f3a7385732d4b3194f62dd0e2a01eb4d4c276543
Alfo5123 <alfredo.delafuente.b@gmail.com>
2018-03-09 17:32:37 +0300

    Added benchmark plotting file

--- code/results/plot.py
+++ code/results/plot.py
@@ -1,8 +1,25 @@
-import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-#Run Soft Q-learning
-sys.path.append('../sql')
-import trainingSQL
-trainingSQL.trainSQL()
+smoothing_window = 20
 
+sql_rewards = np.load('SQL-Rewards.npy')
+dqn_rewards = np.load('DQN-Rewards.npy')
+a3c_rewards = np.load('A3C-Rewards.npy')
 
+sql_smooth = pd.Series(sql_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+dqn_smooth = pd.Series(dqn_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+a3c_smooth = pd.Series(a3c_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+
+plt.figure(figsize = (20,10))
+plt.title('Benchmark Training Results', fontsize = '18')
+plt.xlabel('Episode Reward (Smoothed)', fontsize = '14')
+plt.ylabel('Reward', fontsize = '14')
+plt.plot(sql_smooth , label="SQL")
+plt.plot(dqn_smooth, label="DQN")
+plt.plot(a3c_smooth, label="A3C")
+plt.legend(loc='best', fontsize = '14')
+plt.savefig('Benchmark1.eps', format='eps', dpi=1000)
+#plt.show()
+#plt.close()
