b4637c5c770d6ac0656b6543f31ef9d226964228
koloskova <anastasiia.koloskova.v@gmail.com>
2018-03-10 14:00:55 +0300

    sql finally working & some updates(plots and results)

--- code/results/plot.py
+++ code/results/plot.py
@@ -4,22 +4,25 @@ import pandas as pd
 
 smoothing_window = 20
 
-sql_rewards = np.load('SQL-Rewards.npy')
-dqn_rewards = np.load('DQN-Rewards.npy')
+sql_rewards = np.load('env1-sql-rewards.npy')
+dqn_rewards = np.load('env1-dqn-rewards.npy')
 a3c_rewards = np.load('A3C-Rewards.npy')
 
-sql_smooth = pd.Series(sql_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
-dqn_smooth = pd.Series(dqn_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
-a3c_smooth = pd.Series(a3c_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
+sql_smooth = pd.Series(sql_rewards).rolling(smoothing_window,
+                            min_periods=smoothing_window).mean()
+dqn_smooth = pd.Series(dqn_rewards).rolling(smoothing_window,
+                            min_periods=smoothing_window).mean()
+a3c_smooth = pd.Series(a3c_rewards).rolling(smoothing_window,
+                            min_periods=smoothing_window).mean()
 
 plt.figure(figsize=(20,10))
 plt.title('Benchmark Training Results', fontsize='18')
 plt.xlabel('Episode Reward (Smoothed)', fontsize='14')
-plt.ylabel('Reward', fontsize = '14')
+plt.ylabel('Reward', fontsize='14')
 plt.plot(sql_smooth, label="SQL")
 plt.plot(dqn_smooth, label="DQN")
 plt.plot(a3c_smooth, label="A3C")
 plt.legend(loc='best', fontsize='14')
 plt.savefig('Benchmark1.eps', format='eps', dpi=1000)
-#plt.show()
-#plt.close()
+# plt.show()
+# plt.close()
--- code/results/testSQL.py
+++ code/results/testSQL.py
@@ -8,15 +8,15 @@ import trainingSQL
 trainingSQL.trainSQL(file_name="env1",
                     env=GridworldEnv(1),
                     batch_size=128,
-                    gamma=0.999,
+                    gamma=0.8,
                     beta=5,
                     eps_start=0.9,
                     eps_end=0.05,
-                    eps_decay=100,
+                    eps_decay=300,
                     is_plot=False,
                     num_episodes=500,
                     max_num_steps_per_episode=1000,
-                    learning_rate=0.0001,
+                    learning_rate=0.001,
                     memory_replay_size=10000,)
 
 
