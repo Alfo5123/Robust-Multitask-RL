87012824531ecf5c0938668710e0a7a101767888
Alfo5123 <alfredo.delafuente.b@gmail.com>
2018-03-16 19:33:09 +0300

    Added gif functionality to utils. To do: adapt DQN and SQL to receive another input

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+import imageio
 import torch
 from torch.autograd import Variable
 use_cuda = torch.cuda.is_available()
@@ -14,6 +16,8 @@ def get_screen(env):
     return screen.unsqueeze(0).unsqueeze(0).type(Tensor)
 
 def play_game(env, agent, max_steps_num=100):
+
+    keyFrames = []
     env.reset()
     plt.ion()
     for steps_done in range(0, max_steps_num):
@@ -21,14 +25,30 @@ def play_game(env, agent, max_steps_num=100):
         state = get_screen(env)
         plt.imshow(state.cpu().squeeze(0).squeeze(0).numpy(),
                    interpolation='none')
+
+        filename = "env4_0" + str(steps_done) + ".png"
+        plt.savefig(filename)
+        keyFrames.append(filename)
+
         plt.draw()
-        plt.pause(0.01)
-        action = agent(Variable(state, volatile=True).type(FloatTensor
-                            )).data.max(1)[1].view(1, 1)
+        plt.pause(0.1)
+        action = agent(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
         _, reward, done, _ = env.step(action[0, 0])
+
         if done:
+            state = get_screen(env)
+            plt.imshow(state.cpu().squeeze(0).squeeze(0).numpy(),interpolation='none')
+
+            filename = "env4_0" + str(steps_done+1) + ".png"
+            plt.savefig(filename)
+            keyFrames.append(filename)
             break
     plt.ioff()
+
+    images = [imageio.imread(fn) for fn in keyFrames]
+    gifFilename = "env4-solution.gif"
+    imageio.mimsave(gifFilename, images, duration=0.3)
+
 
 
 def plot_durations(episode_durations, mean_durations):
